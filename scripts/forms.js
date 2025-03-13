be36940e5b1d51e3bae072521406d302a75cbb9b
soudael <soudaelyn@gmail.com>
2025-03-13 16:51:22 -0300

    Projeto finalizado

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,3 +1,28 @@
+var img = document.getElementById("seta-voltar");
+img.src = "./assets/icons/SetaEsquerdaCinza.svg";
+
+function trocarSetaParaLaranja() {
+    var img = document.getElementById("seta-voltar");
+
+    setTimeout(function () {
+        img.src = "./assets/icons/SetaEsquerdaLaranja.svg";
+        img.style.opacity = "1"; // Volta com efeito suave
+    }); // Pequeno atraso para evitar piscar
+}
+
+function trocarSetaParaCinza() {
+    var img = document.getElementById("seta-voltar");
+
+    setTimeout(function () {
+        img.src = "./assets/icons/SetaEsquerdaCinza.svg";
+        img.style.opacity = "1";
+    });
+}
+
+var img = document.getElementById("link-voltar");
+img.addEventListener("mouseover", trocarSetaParaLaranja);
+img.addEventListener("mouseout", trocarSetaParaCinza);
+
 function verificarInput(event) {
     let input = event.target; // seleciona apenas o input que deu origem à execução desse evento
     if (input.value.trim() === "") { // acontece caso não haja nada escrito no input
@@ -12,6 +37,7 @@ function adicionarRedeSocial() {
     let novoIdentificador = "rede-" + (qtdInputs + 1); // cria um novo ID baseado na quantidade de inputs até o momento
 
     let novoInput = document.createElement("input"); // define os atributos do novo input
+    novoInput.setAttribute("class", "forms__input");
     novoInput.setAttribute("id", novoIdentificador);
     novoInput.setAttribute("name", "rede-social");
     novoInput.setAttribute("placeholder", "Insira um link");
@@ -26,3 +52,69 @@ function adicionarRedeSocial() {
 
     novoInput.focus(); // habilita a escrita dentro do input logo que é criado
 }
+
+/* ------------------------------- FORMATAÇÃO ------------------------------- */
+
+function formatarData(input) {
+    let value = input.value.replace(/\D/g, "");
+
+    if (value.length > 8) {
+        value = value.slice(0, 8);
+    }
+
+    value = value.replace(/(\d{2})(\d+)/, "$1/$2");
+    value = value.replace(/(\d{2}\/\d{2})(\d+)/, "$1/$2");
+
+    input.value = value;
+}
+
+function formatarCPF(input) {
+    let value = input.value.replace(/\D/g, "");
+
+    if (value.length > 11) {
+        value = value.slice(0, 11);
+    }
+
+    value = value.replace(/(\d{3})(\d+)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d+)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+
+    input.value = value;
+}
+
+function formatarTel(input) {
+    let value = input.value.replace(/\D/g, "");
+
+    if (value.length > 11) {
+        value = value.slice(0, 8);
+    }
+
+    value = value.replace(/(\d{2})(\d+)/, "($1) $2");
+    value = value.replace(/(\d{5})(\d+)$/, "$1-$2");
+
+    input.value = value;
+}
+
+function formatarCEP(input) {
+    let value = input.value.replace(/\D/g, "");
+
+    if (value.length > 8) {
+        value = value.slice(0, 8);
+    }
+
+    value = value.replace(/(\d{5})(\d+)/, "$1-$2")
+
+    input.value = value;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const fileInputs = document.querySelectorAll('input[type="file"]');
+    fileInputs.forEach(input => {
+        const labelText = document.querySelector(`label[for="${input.id}"] .forms__upload__label`);
+        if (labelText) {
+            input.addEventListener("change", function() {
+                labelText.textContent = this.files.length > 0 ? this.files[0].name : "Clique aqui para selecionar o arquivo";
+            });
+        }
+    });
+});
